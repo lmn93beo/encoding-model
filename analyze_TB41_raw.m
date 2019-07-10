25399bc167069dd53e6af60b152de3fa1e4995fa
Nhat Le <lmn93beo@yahoo.com>
2019-07-10 09:55:49 -0400

    Added file for analyzing TB41 raw traces

---
+++ analyze_TB41_raw.m
@@ -0,0 +1,230 @@
+options.f_folder_name = 'C:\Users\Sur lab\Dropbox (MIT)\Sur\ExternalCode\encoding-model';
+options.b_file_name = 'C:\Users\Sur lab\Dropbox (MIT)\Sur\ExternalCode\encoding-model\behavior_file_TB41.mat';
+
+options.neuropil = 1;
+options.neuropil_subt = 1;
+options.dt = [-3 5];
+
+[trials_dff, trials_z_dff, dff, z_dff, ix, ixCue] = getTrials_tb(options);
+ncells = size(z_dff, 1);
+
+%% Visualize
+imagesc(dff);
+%plot(dff(1,:));
+hold on;
+for i = 1:numel(ix)
+   plot([ix(i) ix(i)], [-100 700], 'b');
+   plot([ixCue(i) ixCue(i)], [-100 700], 'r');
+end
+
+%% Load and preprocess behavior file
+load behavior_file_TB41.mat
+%1 = r, 2 = l stim location
+
+ntrials = sum(ix < 9000);
+choice = data.response.choice(1:ntrials);
+%ntrials = numel(choice);
+
+opp_contrasts = data.stimuli.opp_contrast(1:ntrials);
+loc = data.stimuli.loc(1:ntrials);
+contrast = data.params.contrast;
+
+% Create the stim vectors                                                  
+lstim = ones(1, ntrials) * contrast;
+rstim = ones(1, ntrials) * contrast;
+
+% TODO: check if 1 means L or R
+lstim(loc == 1) = opp_contrasts(loc == 1);
+rstim(loc == 2) = opp_contrasts(loc == 2);
+difficulty = abs(lstim - rstim);
+
+modified_RT = data.response.reward * 0;
+modified_RT(data.response.reward > 0) = data.response.rewardtimes;
+dt_rew = modified_RT - data.response.earlyCueTime;
+dt_rew(~data.response.reward) = 0;
+dt_rew_frames = floor(dt_rew / 0.2);
+
+% For ball movement
+% samples_start = cell2mat(data.response.samples_start);
+%dX = data.response.dx;
+ballT = data.response.time;
+ballT = ballT - ballT(1); % So that ballT starts at 0
+fs = (length(ballT) - 1) / (ballT(end) - ballT(1));
+dx_filt = lowpass(data.response.dx, 0.2);
+
+
+%%
+% Subsample the trials with opp_contrast == 0.32 and choice ~= 5
+goodtrials = (choice(2:end) ~= 5) & (choice(1:end-1) ~= 5);
+goodtrials = logical([0 goodtrials]);
+goodtrialsID = find(goodtrials);
+%goodtrials = (choice ~= 5);
+%goodtrials(1) = 0;
+%goodtrials(258) = 0; %exclude this trial because neural activity has nan's
+%TODO: consider not excluding this
+
+% lstim = lstim(goodtrials);
+% rstim = rstim(goodtrials);
+% loc = loc(goodtrials);
+% choice = choice(goodtrials);
+% opp_contrasts = opp_contrasts(goodtrials);
+
+% prevchoice = data.response.choice(goodtrialsID - 1);
+
+
+% samples_start = samples_start(goodtrials);
+
+
+
+%% Make the predictors
+ntrialsgood = sum(goodtrials) - 1;
+trial_lengths = diff(ixCue);
+
+% Kernel for cue onset
+for i = 1:ntrialsgood
+    T = trial_lengths(goodtrialsID(i));
+    cue_onset = zeros(T, 1);
+    cue_onset(1) = 1;
+    cue_onsetCells{i, 1} = cue_onset;
+end
+
+% Kernel for L-stim and R-stim onset
+for i = 1:ntrialsgood
+    T = trial_lengths(goodtrialsID(i));
+    left_onset = zeros(T, 1);
+    right_onset = zeros(T, 1);
+    if loc(goodtrialsID(i)) == 2 % L-stim
+        left_onset(ix(goodtrialsID(i)) - ixCue(goodtrialsID(i)) + 1) = 1;
+    elseif loc(goodtrialsID(i)) == 1 % R-stim
+        right_onset(ix(goodtrialsID(i)) - ixCue(goodtrialsID(i)) + 1) = 1;
+    else
+        error('Invalid trial. Only l/r choices are acceptable');
+    end
+    
+    left_onsetCells{i, 1} = left_onset;
+    right_onsetCells{i, 1} = right_onset;
+end
+
+% Reward times
+rewards = zeros(ntrialsgood, T);
+for i = 1:ntrialsgood
+    T = trial_lengths(goodtrialsID(i));
+    reward_arr = zeros(T, 1);
+    if dt_rew_frames(goodtrialsID(i)) > 0
+        reward_arr(dt_rew_frames(goodtrialsID(i)) + 1) = 1;
+    end
+    rewardsCell{i, 1} = reward_arr; 
+end
+
+% %%    
+% Build the time information
+balldata = cell(ntrialsgood, 1);
+for i = 1:ntrialsgood
+    T = trial_lengths(goodtrialsID(i));
+    startT = data.response.earlyCueTime(goodtrialsID(i));
+    endT = data.response.earlyCueTime(goodtrialsID(i) + 1);
+    tpoints = linspace(startT, endT, T + 1);
+    
+    % Find the points of dx_filt that are closest to tpoints
+    [~,ixdx] = min(abs(tpoints - ballT),[],1);
+    balldata{i} = dx_filt(ixdx(1:end-1));
+end   
+
+%% Balldata visualization
+start = ixdx(1);
+final = ixdx(end);
+figure;
+plot(start:final, dx_filt(start:final));
+hold on;
+plot(ixdx, dx_filt(ixdx), 'o');
+
+%%
+figure;
+plot(ballT - ballT(1), data.response.dx);
+hold on;
+for i = 1:ntrials
+    startT = data.response.trialstart(i);
+    cueT = data.response.earlyCueTime(i);
+    plot([cueT, cueT], [-20 20], 'k');
+    if choice(i) == 1
+        plot([startT, startT], [-20 20], 'r');
+    elseif choice(i) == 2
+        plot([startT, startT], [-20 20], 'b');
+    end
+    
+end
+plot([1 ballT(end)], [-15 -15], 'k--');
+plot([1 ballT(end)], [15 15], 'k--');
+
+
+%% Visualize events
+figure;
+hold on;
+for i = 1:ntrialsgood
+    T = numel(cue_onsetCells{i});
+    plot((1:T) + 50 * i, cue_onsetCells{i} * 20, 'b');
+    plot((1:T) + 50 * i, left_onsetCells{i} * 20, 'r');
+    plot((1:T) + 50 * i, right_onsetCells{i} * 20, 'g');
+    plot((1:T) + 50 * i, rewardsCell{i} * 20, 'k');
+    plot((1:T) + 50 * i, balldata{i}, 'k--');
+end
+
+ylim([-30 30])
+
+%% Make whole-trial variables
+choiceGood = choice(goodtrialsID);
+prevchoice = choice(goodtrialsID - 1);
+difficultyGood = difficulty(goodtrialsID);
+ 
+
+%% Make the predictor matrix
+pred_types_cell = {'event', 'event', 'event', 'event', 'whole-trial', 'whole-trial', 'whole-trial', 'continuous'};
+groupings = {1, 2, 3, 4, 5, 6, 7, 8};
+
+[pred_allmat,pred_inds_cell,grouped_var_types] = make_predictor_matrix_generalcase({cue_onsetCells, left_onsetCells, right_onsetCells,...
+     rewardsCell, choiceGood, prevchoice, difficultyGood, balldata}, ...
+    pred_types_cell, groupings);
+
+%% Make the neural matrix (response)
+% Make the activity matrix
+goodtrialsID = find(goodtrials);
+neural_act_mat = cell(ntrialsgood, 1);
+for i = 1:ntrialsgood
+    T = trial_lengths(goodtrialsID(i));
+    neural_act_mat{i} = z_dff(:, ixCue(goodtrialsID(i)) : ixCue(goodtrialsID(i) + 1) - 1)';
+end
+%%
+
+neural_matmat = cell2mat(neural_act_mat);
+predall_matmat = cell2mat(pred_allmat);
+
+figure;
+cellID = 1;
+plot(neural_matmat(:,cellID));
+hold on;
+for i = 1:180
+    plot([i * 41, i * 41], [-2 10], '--');
+end
+
+%% Do encoding model
+approach = 'norefit';
+
+%pred_types_cell_group = {'event', 'whole-trial', 'whole-trial', 'whole-trial', 'continuous'};
+
+[relative_contrib,~,r2val] = process_encoding_model(pred_allmat, pred_inds_cell, neural_act_mat, pred_types_cell,approach);
+
+%% Visualize
+means = nanmean(relative_contrib, 1);
+stds = nanstd(relative_contrib, 1) / sqrt(size(relative_contrib, 1));
+figure;
+bar(means);
+hold on;
+errorbar(1:size(relative_contrib, 2), means, stds, '.', 'Color', 'b');
+%hold on;
+xticks(1:size(relative_contrib, 2));
+
+xticks(1:size(relative_contrib, 2));
+factors = {'Cue', 'LeftStim', 'RightStim', 'Reward', 'Choice', 'PrevChoice', 'Difficulty', 'Movement'};
+ylabel('Relative Contribution');
+
+xticklabels(factors);
